98f1db5fa0381073764a47a52707ca63553a69d3
crumbsandpennies <justjoshingyou03@gmail.com>
2025-04-24 14:11:37 -0400

    add bot activity status

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
-import { Events } from 'discord.js';
+import { ActivityType, Events } from 'discord.js';
 
 export default {
   name: Events.ClientReady,
   once: true,
   execute(client) {
     console.log(`Ready! Logged in as ${client.user.tag}`);
+    client.user.setActivity("with your mom", { type: ActivityType.Playing });
   },
 };
